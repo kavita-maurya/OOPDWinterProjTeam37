b9d3edddf0a264dc09db5467bd42de209b1f45f2
priyanka15047 <priyanka15047@iiitd.ac.in>
2015-12-08 13:56:13 +0530

    first project

---
+++ Teammmmmm/src/main/java/oopd/Teammmmmm/App.java
@@ -0,0 +1,13 @@
+package oopd.Teammmmmm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
